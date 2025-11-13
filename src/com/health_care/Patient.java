3630f6cee4bf17102842fc609ae976b705458693
Lalsab10 <lalsabhuddar913@gmail.com>
2025-11-13 08:31:23 +0530

    created class

--- src/com/health_care/Patient.java
+++ src/com/health_care/Patient.java
@@ -2,4 +2,8 @@ package com.health_care;
 
 public class Patient {
 
+	void diseas() {
+		System.out.println("suffering from late night sleep");
+	}
+	
 }
---
+++ src/com/health_care/locations.java
@@ -0,0 +1,6 @@
+package com.health_care;
+
+public class locations {
+
+	
+}
